1395a9854ed5324b6ba8676c9bb5a8475cd414cb
ChoudharyRenu <30753085+ChoudharyRenu@users.noreply.github.com>
2018-03-05 14:41:46 +0530

    Add files via upload

---
+++ exact.py
@@ -0,0 +1,19 @@
+with open("f1.txt") as f1,open ("f2.txt") as f2:
+ countL=0
+ countC=0
+ for i,line1 in enumerate(f1) and for line2 in enumerate(f2):
+  for j in range i to i+9:
+    if line1 == line2:
+       countL = countL + 1;
+  if countL == 10:
+   countC = countC + 1;
+
+print "total number of exact clones is",countC
+
+
+#the problem is with the syntax
+#I m trying to access the lines of both files and "i" is to keep the track of line number so that
+# we can generate a new window as i gets incremented(window size i have taken 10),
+#every time we check that if countL is equal to the window size then we increment 
+# countC(exactclone)
+#I googled but could not find anything related to this please try fixing it no
---
+++ freqExact.py
@@ -0,0 +1,74 @@
+#import collections
+#with open("f1.txt") as f1,open ("f2.txt") as f2:
+#	for i,line in enumerate(f1):
+#		for j in range (i,i+4):
+#			wordcount = collections.Counter()
+#			wordcount.update(line.split("\n"))
+#		for k,v in wordcount.iteritems():
+#  			print k,v
+#		print "i value is updated",i
+
+
+with open("f1.txt") as f1:
+ wordlist1=list()
+ line=f1.readline()
+ #numlines=sum(1 for line in ("f1.txt"))
+ while line:
+  words=line.split("\n")
+  wordlist1.append(words[0])
+  line=f1.readline()
+  #print words[0]
+
+print wordlist1
+
+freq1 = {}
+for item in wordlist1:
+ if item in freq1:
+  freq1[item] = freq1.get(item)+1
+ else:
+  freq1[item] = 1
+
+for k,v in freq1.items():
+ print(str(k) + ':' + str(v))
+
+
+with open("f2.txt") as f2:
+ wordlist2=list()
+ line=f2.readline()
+ #numlines=sum(1 for line in ("f1.txt"))
+ while line:
+  words=line.split("\n")
+  wordlist2.append(words[0])
+  line=f2.readline()
+  #print words[0]
+
+freq2 = {}
+for item in wordlist1:
+ if item in freq2:
+  freq2[item] = freq2.get(item)+1
+ else:
+  freq2[item] = 1
+
+for k,v in freq1.items():
+ print(str(k) + ':' + str(v))
+
+count1=0
+count2=0
+for x,y in zip(freq1.iteritems(),freq2.iteritems()):
+	if x == y:
+		print "exact match"
+		count1=count1+1
+	
+	else:
+		print "no match"
+		count2=count2+1
+
+if count2 == 0:
+	print "exact clones"
+else :
+	print "not exact clone"
+
+#this is calculating the frequency of mnemonics in the textfile
+#how can we use this to find the frequency of each window(region) in a for loop,so that we can 
+#compare  the frequencies of both of the windows and then can decide whether they are exactclone 
+# or not
